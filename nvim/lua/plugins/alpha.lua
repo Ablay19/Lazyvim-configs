0e9d55f29055b9249837094886fdb6f2329bccfa
Ablay19 <Ablay19@github.com>
2025-07-01 21:06:36 +0000

    I fix a lot of the issues those are relatef to the crud for the activities

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,31 @@
+return {
+  "goolord/alpha-nvim",
+  -- This disables LazyVim's default alpha dashboard config
+  enabled = true,
+  opts = false,
+
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.startify")
+
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+    }
+
+    alpha.setup(dashboard.opts)
+  end,
+}
